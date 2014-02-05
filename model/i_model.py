fecf9228f39c35336d254236633ed0c72cafa7d9
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-02-05 22:26:25 +0100

    add model.motor_primitive.py with basis functions implementation

--- model/i_model.py
+++ model/i_model.py
@@ -1,6 +1,7 @@
 import numpy
 
 import imle as imle_
+from gmminf import GMM
 import utils
 
 class InterestModel(object):
@@ -11,14 +12,14 @@ class InterestModel(object):
     def sample(self):
         raise NotImplementedError
 
-    def update(self):
+    def update(self, x, comp):
         raise NotImplementedError
 
 class RandomInterest(InterestModel):
     def sample(self):
         return utils.rand_bounds(self.bounds)
 
-    def update(self, x, err):
+    def update(self, x, comp):
         pass
 
 class ProgressInterest(InterestModel):
@@ -29,7 +30,7 @@ class ProgressInterest(InterestModel):
                                sigma0=0.2**2., Psi0=psi0, p0=0.3, multiValuedSignificance=0.5)
         self.scale_t = 2. * 0.2/numpy.sqrt(sigma0)
         self.t = 0.
-        self.gmm = imle_.GMM(n_components=1, covariance_type='full')
+        self.gmm = GMM(n_components=1, covariance_type='full')
         self.gmm.weights = [1.0]
         self.gmm.means_ = numpy.zeros((1, self.ndims+2))
 #TO FIX: covars_ in fct of sigma0 and Psi0 :
@@ -61,4 +62,60 @@ class ProgressInterest(InterestModel):
         #gmm_choice = gmm_choice.inference([], [0], []) 
         return gmm_choice
 
+class GmmInterest(InterestModel):
+    def __init__(self, bounds, sigma0, psi0):
+        InterestModel.__init__(self, bounds)
 
+        self.scale_t = 2. * 0.2/numpy.sqrt(sigma0)
+        self.t = 0.
+        self.gmm = GMM(n_components=12, covariance_type='full')
+        self.gmm.weights_ = [1.0] * self.gmm.n_components
+        self.gmm.means_ = numpy.zeros((self.gmm.n_components, self.ndims+2))
+#TO FIX: covars_ in fct of sigma0 and Psi0 :
+        variances = numpy.hstack((1. , (1./12.) * (0.1 * (bounds[1,:] - bounds[0,:])) ** 2, psi0))
+        self.gmm.covars_ = numpy.array([numpy.diag(variances)] * self.gmm.n_components)
+        self.data = []
+        self.nb_data = 1000
+        self.n_t = 0
+
+    def sample(self):
+        x = self.gmm_interest().sample()
+        x = numpy.maximum(x, self.bounds[0, :])
+        x = numpy.minimum(x, self.bounds[1, :])
+        return x.T
+
+    def update(self, x, comp):
+        if len(self.data) > self.nb_data:
+            self.data.remove(self.data[0])
+        self.data.append(numpy.hstack(([self.t], x.flatten(), [comp])))
+        self.t += self.scale_t
+        if abs(self.t % (self.nb_data * self.scale_t / 4.)) < self.scale_t:
+            print 'upd', self.t 
+            self.update_gmm()
+        return self.t, x, comp
+
+    def update_gmm(self):
+        self.gmm =  GMM(n_components=12, covariance_type='full')
+        self.gmm.fit(numpy.array(self.data))
+
+    def gmm_interest(self):
+        cov_t_c = numpy.array([self.gmm.covars_[k,0,-1] for k in range(self.gmm.n_components)])
+        cov_t_c = numpy.exp(cov_t_c)
+        #cov_t_c[cov_t_c <= 1e-100] = 1e-100
+        gmm_choice = self.gmm.inference([0], range(1, self.ndims + 1), [self.t])
+        gmm_choice.weights_ = cov_t_c
+        gmm_choice.weights_ /= numpy.array(gmm_choice.weights_).sum()
+        #gmm_choice = gmm_choice.inference([], [0], []) 
+        return gmm_choice
+
+
+class BayesOptInterest(InterestModel):
+    def __init__(self, bounds):
+        InterestModel.__init__(self, bounds)
+        data = []
+
+    def sample(self):
+        raise NotImplementedError
+
+    def update(self, x, comp):
+        raise NotImplementedError
---
+++ model/motor_primitive.py
@@ -0,0 +1,14 @@
+from gaussian import Gaussian
+from numpy import linspace, array, arange, tile, dot
+
+class BasisFunctions(object):
+    def __init__(self, n_basis, duration, dt, sigma):
+        self.n_basis = n_basis
+        means = linspace(0, duration, n_basis) 
+        variances = 1. / (sigma * n_basis)**2
+        gaussians = [Gaussian(array([means[k]]), array([[variances]])) for k in range(len(means))] 
+        self.x = arange(0., duration, dt)
+        y = array([gaussians[k].normal(self.x.reshape(-1,1)) for k in range(len(means))] ) 
+        self.z = y / tile(sum(y,0), (n_basis,1))
+    def trajectory(self, weights):
+        return dot(weights, self.z)
