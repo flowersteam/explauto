da38773cb57a40034568fcaccf9f2b4bfc4003d5
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-04-17 12:41:54 +0200

    one more

--- explauto/environment/adapter.py
+++ explauto/environment/adapter.py
@@ -1,17 +1,20 @@
 import importlib
 import models
+from ..utils.config import Configuration 
 import numpy as np
 
 class Robot(object):
-    def __init__(self, exp):
-        self.s_feats = tuple(range(exp.s_ndims))
-        self.m_feats = tuple(range(-exp.m_ndims, 0))
+    def __init__(self, env):
+        #self.conf = Configuration(env.params)
+        self.s_feats = tuple(range(env.s_ndims))
+        self.m_feats = tuple(range(-env.m_ndims, 0))
         
-        self.m_bounds = tuple([tuple(exp.ms_bounds[:, d].T) for d in exp.m_dims])
-        self.env = exp.env
+        self.m_bounds = tuple([tuple(env.bounds[:, d].T) for d in env.m_dims])
+        self.env = env
         
     def execute_order(self,order):
-        return tuple(self.env.execute(order)[1].flatten())
+        self.env.next_state(order)
+        return tuple(self.env.state[self.env.s_dims])
 
     def test_set(self, n_tests):
         return models.testbed.testcase.uniform_sensor_testcases(self, n_tests)
