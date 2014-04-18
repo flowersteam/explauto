b9049ae47d150109f53297d03ee13e376ab8d830
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-04-18 15:05:00 +0200

    adaptors to models

--- explauto/environment/adapter.py
+++ explauto/environment/adapter.py
@@ -1,22 +1,32 @@
+""" Adaptors to Fabien Benureau's models library. """
+
 import importlib
 from ..utils.config import Configuration
 import numpy as np
 
-from ..third_party.models.models import testbed
+#from ..third_party.models.models import testbed
 
+def configuration(env_or_ag):
+    s_feats = tuple(range(env_or_ag.s_ndims))
+    m_feats = tuple(range(-env_or_ag.m_ndims, 0))
+    m_bounds = tuple([(env_or_ag.m_mins[d], env_or_ag.m_maxs[d]) for d in conf['m_dims']])
+    return m_feats, s_feats, m_bounds
 
 class Robot(object):
     def __init__(self, env):
-        #self.conf = Configuration(env.params)
-        self.s_feats = tuple(range(env.s_ndims))
-        self.m_feats = tuple(range(-env.m_ndims, 0))
-
-        self.m_bounds = tuple([tuple(env.bounds[:, d].T) for d in env.m_dims])
+        self.m_feats, self.s_feats, self.m_bounds = configuration(env)
         self.env = env
 
     def execute_order(self,order):
         self.env.next_state(order)
         return tuple(self.env.state[self.env.s_dims])
 
-    def test_set(self, n_tests):
-        return testbed.testcase.uniform_sensor_testcases(self, n_tests)
+    #def test_set(self, n_tests):
+        #return testbed.testcase.uniform_sensor_testcases(self, n_tests)
+
+class Learner(object):
+    def __init__(self, ag):
+        self.m_feats, self.s_feats, self.m_bounds = configuration(ag)
+        self.ag = ag
+
+
--- explauto/experiment/experiment.py
+++ explauto/experiment/experiment.py
@@ -2,16 +2,19 @@ from numpy import zeros
 from ..utils.observer import Observable
 
 class Experiment(Observable):
-    def __init__(self, env, ag, n_records = 100000):
+    def __init__(self, env, ag, n_records=100000, evaluate_at=[]):
         self.env = env
         self.ag = ag
         #env.inds_in = inds_in
         #env.inds_out = inds_out
         self.records = zeros((n_records, env.state.shape[0]))
         self.i_rec = 0
+        self.evaluate_at = evaluate_at
 
     def run(self, n_iter = 1):
         for _ in range(n_iter):
+            #if i_rec in evaluate_at:
+                #self.evaluation.evaluate(self.env, self.ag, self.testset, self. 
             self.env.next_state(self.ag.next_state(self.env.read()))
             #self.env.write(self.ag.produce())
             #self.env.next_state()
