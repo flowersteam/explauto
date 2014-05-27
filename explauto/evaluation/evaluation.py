d5c9db002938e1d5768337d50aa775c014b2c384
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-05-27 17:53:18 +0200

    Adapt evaluation to new testcases

--- explauto/evaluation/evaluation.py
+++ explauto/evaluation/evaluation.py
@@ -1,40 +1,50 @@
 from numpy import array, hstack
+from numpy import linalg
 
 from ..third_party.models.models.testbed import Testbed
 from ..third_party.models_adaptors import Learner, Robot
 
 
 class Evaluation(object):
-    def __init__(self, ag, env, testcases=None, n_samples=100, mode='inverse'):
+    def __init__(self, ag, env, testcases, n_samples=100, mode='inverse'):
         self.ag = ag
-        learner = Learner(ag)
-        robot = Robot(env, log=False)
-        self.tester = Testbed(robot, learner, testcases)
+        self.env = env
+        # learner = Learner(ag)
+        # robot = Robot(env, log=False)
+        # self.tester = Testbed(robot, learner, testcases)
         self.mode = mode
-        self.errors = []
+        # self.errors = []
 
-        if testcases is None:
-            if mode not in ('forward', 'inverse'):
-                raise ValueError('mode should be "forward" or "inverse"'
-                                 '(general prediction coming soon)')
+        if mode not in ('inverse'):
+            raise ValueError('mode should be "inverse"'
+                             '("forward" and "general" predictions coming soon)')
+        self.testcases = testcases
 
-            tests = getattr(self.tester,
-                            'uniform_motor' if mode == 'forward' else 'uniform_sensor')
-            tests(n_samples)
+            # tests = getattr(self.tester,
+                            # 'uniform_motor' if mode == 'forward' else 'uniform_sensor')
+            # tests(n_samples)
 
     def evaluate(self):
         mode = self.ag.sensorimotor_model.mode
         self.ag.sensorimotor_model.mode = 'exploit'
-        if self.mode == 'forward':
-            self.errors = self.tester.run_forward()
-        elif self.mode == 'inverse':
-            self.errors = self.tester.run_inverse()
+        if self.mode == 'inverse':
+            errors = []
+            for s_g in self.testcases:
+                m = self.ag.infer(self.ag.conf.s_dims, self.ag.conf.m_dims, s_g)
+                self.env.update(m, log=False)
+                s = self.env.state[self.env.conf.s_dims]
+                errors.append(linalg.norm(s_g - s))
+        else:
+            raise ValueError('mode should be "inverse"'
+                             '("forward" and "general" predictions coming soon)')
+
         self.ag.sensorimotor_model.mode = mode
-        return self.errors
+        return errors
 
     def plot_testcases(self, ax, dims, **kwargs_plot):
         plot_specs = {'marker': 'o', 'linestyle': 'None'}
         plot_specs.update(kwargs_plot)
-        test_array = array([hstack((m, s)) for m, s in self.tester.testcases])
-        test_array = test_array[:, dims]
-        ax.plot(*(test_array.T), **plot_specs)
+        # test_array = array([hstack((m, s)) for m, s in self.tester.testcases])
+        # test_array = test_array[:, dims]
+        # ax.plot(*(test_array.T), **plot_specs)
+        ax.plot(*(self.testcases.T), **plot_specs)
