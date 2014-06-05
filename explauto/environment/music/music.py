98e651d2a0b540c1b99989308e940e942f1e6993
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-06-05 14:03:48 +0200

    plot_boxes in MusicEnvironment (first trial)

--- explauto/environment/music/music.py
+++ explauto/environment/music/music.py
@@ -1,6 +1,7 @@
-import time
-from numpy import hstack
-from numpy import random
+# import time
+from os.path import basename
+from itertools import product, combinations
+from numpy import hstack, random, array
 
 from ...utils.sound.recorder import Recorder
 from ...utils.sound.player import Sampler
@@ -21,7 +22,7 @@ class MusicEnvironment(Environment):
     use_process = False
 
     def __init__(self, base_environment,
-                 boxes, sound_samples, sound_analyser, analyser_noise, sound_mins, sound_maxs):
+                 boxes, sound_samples, sound_analyser, analyser_noise, sound_mins, sound_maxs, sample_labels=None):
 
         Environment.__init__(self, base_environment.conf.m_mins,
                              base_environment.conf.m_maxs,
@@ -34,6 +35,10 @@ class MusicEnvironment(Environment):
         self.recorder = Recorder()
         self.analyser = sound_analyser
         self.noise = analyser_noise
+        if sample_labels is not None:
+            self.labels = sample_labels
+        else:
+            self.labels = [basename(s) for s in sound_samples]
 
     def compute_motor_command(self, m_ag):
         return self.env.compute_motor_command(m_ag)
@@ -48,4 +53,16 @@ class MusicEnvironment(Environment):
         s = self.recorder.data
         sound_value = self.analyser(s, r) + self.noise * random.randn()
         return hstack((hand_pos, sound_value))
+
+    def _plot_box(self, ax, i, **kwargs_plot):
+        box = array(self.boxes[i]).T
+        [ax.plot(*zip(s1, s2), color="b")
+         for s1, s2 in list(combinations(product(*zip(*box)), 2))
+         if (array(s1) == array(s2)).sum() == len(s1) - 1]
+
+
+    def plot_boxes(self, ax, **kwargs_plot):
+        for i in range(len(self.boxes)):
+                self._plot_box(ax, i)
+                # ax.text(*(array(self.boxes[i])[:, 0]), text='test')
 
