5ad43b3a79afea1b5e75982e939e233f9ef4c691
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-07-16 19:30:02 +0200

    modify musical_arm

--- explauto/environment/musical_arm/__init__.py
+++ explauto/environment/musical_arm/__init__.py
@@ -4,14 +4,14 @@ from ..simple_arm import make_arm_config
 from .musical_arm import MusicalArm
 from ... import ExplautoNoTestCasesError
 
-
+boxes2D_1 = [[[-0.6, -0.2], [-0.9, -0.6]], [[-0.6, -0.2], [0.6, 0.9]], [[0.    , 0.4], [-0.15, 0.15]]]
 x_min, y_min, x_max, y_max = 0., -1., 0.5, -0.1
-pitches = [-0.6, 0.3, 0.5, 0.7, 0.9]
-noises = [0.3] + [0.1] * (len(pitches) - 1)
+pitches = [1., 2., 3.]
+noises = [0.1] * (len(pitches))
 config = make_arm_config(15, pi/3, array([-1., -1.]), array([1., 1.]), 1.5, 0.02)
 config['pitches'] = pitches
 config['pitch_noises'] = noises
-config['box'] = x_min, y_min, x_max, y_max
+config['boxes'] = boxes2D_1  #  x_min, y_min, x_max, y_max
 
 environment = MusicalArm
 configurations = {'default': config}
--- explauto/environment/musical_arm/musical_arm.py
+++ explauto/environment/musical_arm/musical_arm.py
@@ -1,4 +1,6 @@
 from numpy import array, linspace, nonzero, hstack
+from itertools import product, combinations
+
 from numpy import random
 
 from .. import Environment
@@ -6,41 +8,61 @@ from ...utils import bounds_min_max
 from ..simple_arm import SimpleArmEnvironment
 
 
-# keyborad bounds
+def is_in_box(box, pos):
+    res = True
+    for d, p in enumerate(pos):
+        res &= p >= box[d][0] and p < box[d][1]
+    return res
 
 
 class MusicalArm(Environment):
     def __init__(self, m_mins, m_maxs, s_mins, s_maxs,
-                 length_ratio, noise, pitches, pitch_noises, box):
+                 length_ratio, noise, pitches, pitch_noises, boxes):
         Environment.__init__(self, m_mins, m_maxs,
                              hstack((s_mins, pitches[0] - pitch_noises[0])),
                              hstack((s_maxs, pitches[-1] + pitch_noises[-1])))
         self.arm = SimpleArmEnvironment(m_mins, m_maxs, s_mins, s_maxs, length_ratio, noise)
         self.pitches = pitches
         self.noises = pitch_noises
-        self.x_min, self.y_min, self.x_max, self.y_max = box
-        self.key_lims = linspace(self.y_min, self.y_max, len(self.pitches))
+        # self.x_min, self.y_min, self.x_max, self.y_max = box
+        # self.key_lims = linspace(self.y_min, self.y_max, len(self.pitches))
+        self.boxes = boxes
 
     def compute_motor_command(self, joint_pos):
         return bounds_min_max(joint_pos, self.conf.m_mins, self.conf.m_maxs)
 
     def compute_sensori_effect(self, joint_pos):
-        x, y = self.arm.compute_sensori_effect(joint_pos)
-        if x < self.x_min or x > self.x_max or y < self.y_min or y > self.y_max:
-            note = 0  # no note
-        else:
-            note = nonzero(y < self.key_lims)[0][0]
+        hand_pos = x, y = self.arm.compute_sensori_effect(joint_pos)
+        to_play = [i for i, b in enumerate(self.boxes) if is_in_box(b, hand_pos)]
+        note = 0 if not to_play else to_play[0]
+        # if x < self.x_min or x > self.x_max or y < self.y_min or y > self.y_max:
+        #     note = 0  # no note
+        # else:
+        #     note = nonzero(y < self.key_lims)[0][0]
         pitch = self.pitches[note]
         pitch += self.noises[note] * random.randn()
         return array([x, y, pitch])
 
-    def plot_keyboard(self, ax, pitches=True, **kwargs_plot):
-        plot_specs = {'color': 'black'}
-        plot_specs.update(kwargs_plot)
-        ax.plot([self.x_min, self.x_min, self.x_max, self.x_max, self.x_min],
-                [self.y_min, self.y_max, self.y_max, self.y_min, self.y_min], **plot_specs)
+    def _plot_box(self, ax, i, **kwargs_plot):
+        box = array(self.boxes[i]).T
+        [ax.plot(*zip(s1, s2), color="b")
+         for s1, s2 in list(combinations(product(*zip(*box)), 2))
+         if (array(s1) == array(s2)).sum() == len(s1) - 1]
 
-        for i, y in enumerate(self.key_lims[:-1]):
-            ax.plot([self.x_min, self.x_max], [y, y], **plot_specs)
-            if pitches:
-                ax.text(self.x_min + 0.1, y + 0.1, str(self.pitches[i + 1])) 
+
+    def plot_boxes(self, ax, **kwargs_plot):
+        for i in range(len(self.boxes)):
+                self._plot_box(ax, i)
+                coord = (array(self.boxes[i]).mean(axis=1))
+                # ax.text(*coord, s=self.labels[i])
+
+    # def plot_keyboard(self, ax, pitches=True, **kwargs_plot):
+    #     plot_specs = {'color': 'black'}
+    #     plot_specs.update(kwargs_plot)
+    #     ax.plot([self.x_min, self.x_min, self.x_max, self.x_max, self.x_min],
+    #             [self.y_min, self.y_max, self.y_max, self.y_min, self.y_min], **plot_specs)
+    #
+    #     for i, y in enumerate(self.key_lims[:-1]):
+    #         ax.plot([self.x_min, self.x_max], [y, y], **plot_specs)
+    #         if pitches:
+    #             ax.text(self.x_min + 0.1, y + 0.1, str(self.pitches[i + 1]))
