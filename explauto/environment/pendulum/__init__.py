fdc1b7d4dd7c5c8c60f94ccf91a81b74a77725c8
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-10-15 12:49:17 +0200

    Update pendulum

--- explauto/environment/pendulum/__init__.py
+++ explauto/environment/pendulum/__init__.py
@@ -2,11 +2,11 @@ from numpy import array
 
 from ...utils import rand_bounds
 from .pendulum import PendulumEnvironment
-from .config import test_config, x_min, x_max, v_min, v_max
+from .config import underactuated_config, x_min, x_max, v_min, v_max
 
 
 environment = PendulumEnvironment
-configurations = {'default': test_config}
+configurations = {'default': underactuated_config, 'underactuated': underactuated_config}
 
 
 def testcases(config_str, n_samples=100):
@@ -14,6 +14,3 @@ def testcases(config_str, n_samples=100):
         return rand_bounds(array([[x_min, v_min], [x_max, v_max]]), n_samples)
     else:
         ExplautoNoTestCasesError("Only works for default configuration")
-
-
-            
--- explauto/environment/pendulum/config.py
+++ explauto/environment/pendulum/config.py
@@ -8,7 +8,7 @@ x_max = pi
 v_min = -2.5
 v_max = 2.5
 
-test_config = dict(m_mins=array(m_ndims*[u_min]),
+underactuated_config = dict(m_mins=array(m_ndims*[u_min]),
                    m_maxs=array(m_ndims*[u_max]),
                    s_mins=array([-pi, v_min]),
                    s_maxs=array([pi, v_max]),
