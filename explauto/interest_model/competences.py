7aec963176ba61d8b4b1eb2aabfc35786d089727
Sébastien Forestier <sebastien.forestier@inria.fr>
2015-04-16 20:33:49 +0200

    option of competence_exp to power the exp

--- explauto/interest_model/competences.py
+++ explauto/interest_model/competences.py
@@ -5,8 +5,8 @@ def competence_dist(target, reached, dist_min=0.1):
     return min(- dist_min, - np.linalg.norm(target - reached))
 
 
-def competence_exp(target, reached, dist_min=0.1):
-    return np.exp(min(- dist_min, - np.linalg.norm(target - reached)))
+def competence_exp(target, reached, dist_min=0.1, power = 1.):
+    return np.exp(power*min(- dist_min, - np.linalg.norm(target - reached)))
 
 
 def competence_bool(target, reached):
