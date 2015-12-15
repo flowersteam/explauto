da2dfa79b98cdbb8c58a6f98e65a2d3ea62ce3e5
Sébastien Forestier <sebastien.forestier@inria.fr>
2015-12-15 17:35:00 +0100

    Pypot dependency optional

--- explauto/environment/__init__.py
+++ explauto/environment/__init__.py
@@ -4,12 +4,14 @@ import importlib
 
 environments = {}
 for mod_name in ['simple_arm', 'pendulum', 'pypot']:
-    module = importlib.import_module('explauto.environment.{}'.format(mod_name))
-    env = getattr(module, 'environment')
-    conf = getattr(module, 'configurations')
-    testcases = getattr(module, 'testcases')
-    environments[mod_name] = (env, conf, testcases)
-
+    try:
+        module = importlib.import_module('explauto.environment.{}'.format(mod_name))
+        env = getattr(module, 'environment')
+        conf = getattr(module, 'configurations')
+        testcases = getattr(module, 'testcases')
+        environments[mod_name] = (env, conf, testcases)
+    except ImportError:
+        pass
 def available_configurations(environment):
     _, env_configs, _ = environments[environment]
     return env_configs
