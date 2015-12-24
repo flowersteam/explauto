b81b43d594c4f8e192ab2e1c0f1032e8d01332ec
Sébastien Forestier <sebastien.forestier@inria.fr>
2015-12-24 11:54:29 +0100

    added environment wrap for using context with a specified environment

---
+++ explauto/environment/context_environment.py
@@ -0,0 +1,47 @@
+
+import numpy as np
+
+from explauto.environment.environment import Environment
+from ..utils import bounds_min_max
+
+
+class ContextEnvironment(Environment):
+
+    def __init__(self, env_cls, env_conf, context_mode):
+        self.rest_position = context_mode['rest_position']
+        self.context_mode = context_mode        
+        self.env = env_cls(**env_conf)          
+        
+        Environment.__init__(self, 
+                            np.hstack((self.env.conf.m_mins, self.context_mode['dm_bounds'][0])), 
+                            np.hstack((self.env.conf.m_maxs, self.context_mode['dm_bounds'][1])),
+                            np.hstack((self.env.conf.s_mins, self.context_mode['ds_bounds'][0])),
+                            np.hstack((self.env.conf.s_maxs, self.context_mode['ds_bounds'][1])))                                            
+        self.reset()
+        
+    def reset(self):
+        self.current_motor_position = np.array(self.rest_position)
+        self.current_sensori_position = np.array(self.env.update(self.current_motor_position, reset=True))
+    
+    def compute_motor_command(self, ag_state):
+        return bounds_min_max(ag_state, self.conf.m_mins, self.conf.m_maxs)
+    
+    def compute_sensori_effect(self, mdm):
+        if self.context_mode['choose_m'] == True:  
+            m = mdm[:len(mdm)/2]
+            dm = mdm[len(mdm)/2:]
+            s = self.env.update(m, reset=False)
+            self.current_motor_position = m
+            self.current_sensori_position = s
+        else:
+            dm = mdm
+        s = self.current_sensori_position
+        self.current_motor_position = self.current_motor_position + np.array(dm)
+        new_sensori_position = np.array(self.env.update(self.current_motor_position, reset=False))
+        ds = new_sensori_position - self.current_sensori_position
+        self.current_sensori_position = new_sensori_position
+        #print "Context environment, m", m, "dm", dm, "s", s, "ds", ds
+        return np.hstack((s, ds))
+    
+    def plot(self, ax, **kwargs):
+        self.env.plot(ax, self.current_motor_position, self.current_sensori_position, **kwargs)
